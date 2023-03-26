a8b59202bce9c32d4dca7de7f77a24b10662b8c4
rickyjooooooe <128315245+rickyjooooooe@users.noreply.github.com>
2023-03-27 02:39:42 +0700

    Add files via upload

---
+++ TakeHomeW4_RickyJonathan_0706022210005/TakeHomeW4_RickyJonathan_0706022210005/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace TakeHomeW4_RickyJonathan_0706022210005
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.comboBoxcountry = new System.Windows.Forms.ComboBox();
+            this.comboBoxteam = new System.Windows.Forms.ComboBox();
+            this.textBoxteamname = new System.Windows.Forms.TextBox();
+            this.textBoxteamcountry = new System.Windows.Forms.TextBox();
+            this.textBoxteamcity = new System.Windows.Forms.TextBox();
+            this.textBoxplayername = new System.Windows.Forms.TextBox();
+            this.textBoxplayernumber = new System.Windows.Forms.TextBox();
+            this.comboBoxpos = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonremove = new System.Windows.Forms.Button();
+            this.buttonaddteam = new System.Windows.Forms.Button();
+            this.buttonaddplayer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soccer Team List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Choose Country";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Choose Team";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(493, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adding Team";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(357, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Team Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(357, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Team Country";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(357, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Team City";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(847, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Adding Players";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(716, 71);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Players Name";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(716, 134);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(112, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Player Number";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(716, 193);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(112, 20);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Player Position";
+            // 
+            // comboBoxcountry
+            // 
+            this.comboBoxcountry.FormattingEnabled = true;
+            this.comboBoxcountry.Location = new System.Drawing.Point(148, 68);
+            this.comboBoxcountry.Name = "comboBoxcountry";
+            this.comboBoxcountry.Size = new System.Drawing.Size(153, 28);
+            this.comboBoxcountry.TabIndex = 11;
+            // 
+            // comboBoxteam
+            // 
+            this.comboBoxteam.FormattingEnabled = true;
+            this.comboBoxteam.Location = new System.Drawing.Point(148, 126);
+            this.comboBoxteam.Name = "comboBoxteam";
+            this.comboBoxteam.Size = new System.Drawing.Size(153, 28);
+            this.comboBoxteam.TabIndex = 12;
+            this.comboBoxteam.SelectedIndexChanged += new System.EventHandler(this.comboBoxteam_SelectedIndexChanged);
+            // 
+            // textBoxteamname
+            // 
+            this.textBoxteamname.Location = new System.Drawing.Point(496, 71);
+            this.textBoxteamname.Name = "textBoxteamname";
+            this.textBoxteamname.Size = new System.Drawing.Size(155, 26);
+            this.textBoxteamname.TabIndex = 13;
+            // 
+            // textBoxteamcountry
+            // 
+            this.textBoxteamcountry.Location = new System.Drawing.Point(496, 131);
+            this.textBoxteamcountry.Name = "textBoxteamcountry";
+            this.textBoxteamcountry.Size = new System.Drawing.Size(155, 26);
+            this.textBoxteamcountry.TabIndex = 14;
+            // 
+            // textBoxteamcity
+            // 
+            this.textBoxteamcity.Location = new System.Drawing.Point(496, 193);
+            this.textBoxteamcity.Name = "textBoxteamcity";
+            this.textBoxteamcity.Size = new System.Drawing.Size(155, 26);
+            this.textBoxteamcity.TabIndex = 15;
+            // 
+            // textBoxplayername
+            // 
+            this.textBoxplayername.Location = new System.Drawing.Point(851, 71);
+            this.textBoxplayername.Name = "textBoxplayername";
+            this.textBoxplayername.Size = new System.Drawing.Size(156, 26);
+            this.textBoxplayername.TabIndex = 16;
+            // 
+            // textBoxplayernumber
+            // 
+            this.textBoxplayernumber.Location = new System.Drawing.Point(851, 134);
+            this.textBoxplayernumber.Name = "textBoxplayernumber";
+            this.textBoxplayernumber.Size = new System.Drawing.Size(156, 26);
+            this.textBoxplayernumber.TabIndex = 17;
+            // 
+            // comboBoxpos
+            // 
+            this.comboBoxpos.FormattingEnabled = true;
+            this.comboBoxpos.Location = new System.Drawing.Point(851, 190);
+            this.comboBoxpos.Name = "comboBoxpos";
+            this.comboBoxpos.Size = new System.Drawing.Size(156, 28);
+            this.comboBoxpos.TabIndex = 18;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(16, 193);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 244);
+            this.listBox1.TabIndex = 19;
+            // 
+            // buttonremove
+            // 
+            this.buttonremove.Location = new System.Drawing.Point(316, 407);
+            this.buttonremove.Name = "buttonremove";
+            this.buttonremove.Size = new System.Drawing.Size(83, 30);
+            this.buttonremove.TabIndex = 20;
+            this.buttonremove.Text = "Remove";
+            this.buttonremove.UseVisualStyleBackColor = true;
+            this.buttonremove.Click += new System.EventHandler(this.buttonremove_Click);
+            // 
+            // buttonaddteam
+            // 
+            this.buttonaddteam.Location = new System.Drawing.Point(497, 247);
+            this.buttonaddteam.Name = "buttonaddteam";
+            this.buttonaddteam.Size = new System.Drawing.Size(113, 32);
+            this.buttonaddteam.TabIndex = 21;
+            this.buttonaddteam.Text = "Add Team";
+            this.buttonaddteam.UseVisualStyleBackColor = true;
+            this.buttonaddteam.Click += new System.EventHandler(this.buttonaddteam_Click);
+            // 
+            // buttonaddplayer
+            // 
+            this.buttonaddplayer.Location = new System.Drawing.Point(851, 247);
+            this.buttonaddplayer.Name = "buttonaddplayer";
+            this.buttonaddplayer.Size = new System.Drawing.Size(110, 32);
+            this.buttonaddplayer.TabIndex = 22;
+            this.buttonaddplayer.Text = "Add Player";
+            this.buttonaddplayer.UseVisualStyleBackColor = true;
+            this.buttonaddplayer.Click += new System.EventHandler(this.buttonaddplayer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 450);
+            this.Controls.Add(this.buttonaddplayer);
+            this.Controls.Add(this.buttonaddteam);
+            this.Controls.Add(this.buttonremove);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBoxpos);
+            this.Controls.Add(this.textBoxplayernumber);
+            this.Controls.Add(this.textBoxplayername);
+            this.Controls.Add(this.textBoxteamcity);
+            this.Controls.Add(this.textBoxteamcountry);
+            this.Controls.Add(this.textBoxteamname);
+            this.Controls.Add(this.comboBoxteam);
+            this.Controls.Add(this.comboBoxcountry);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox comboBoxcountry;
+        private System.Windows.Forms.ComboBox comboBoxteam;
+        private System.Windows.Forms.TextBox textBoxteamname;
+        private System.Windows.Forms.TextBox textBoxteamcountry;
+        private System.Windows.Forms.TextBox textBoxteamcity;
+        private System.Windows.Forms.TextBox textBoxplayername;
+        private System.Windows.Forms.TextBox textBoxplayernumber;
+        private System.Windows.Forms.ComboBox comboBoxpos;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonremove;
+        private System.Windows.Forms.Button buttonaddteam;
+        private System.Windows.Forms.Button buttonaddplayer;
+    }
+}
+
---
+++ TakeHomeW4_RickyJonathan_0706022210005/TakeHomeW4_RickyJonathan_0706022210005/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static TakeHomeW4_RickyJonathan_0706022210005.Form1;
+using System.Xml.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace TakeHomeW4_RickyJonathan_0706022210005
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxcountry.SelectedIndexChanged += comboBoxcountry_SelectedIndexChanged;
+        }
+
+        public class Player
+        {
+            string playername;
+            string playernum;
+            string playerpos;
+            public string playerName
+            {
+                get { return playername; }
+                set { playername = value; }
+            }
+            public string playerNum
+            {
+                get { return playernum; }
+                set { playernum = value; }
+            }
+            public string playerPos
+            {
+                get { return playerpos; }
+                set { playerpos = value; }
+            }
+        }
+
+        public class Team
+        {
+            string teamname;
+            string teamcountry;
+            string teamcity;
+            public List<Player> Players = new List<Player>();
+            public string TeamName
+            {
+                get { return teamname; }
+                set { teamname = value; }
+            }
+            public string TeamCountry
+            {
+                get { return teamcountry; }
+                set { teamcountry = value; }
+            }
+            public string TeamCity
+            {
+                get { return teamcity; }
+                set { teamcity = value; }
+            }
+            public void addingplayer(string name, string num, string pos)
+            {
+                Player playerz = new Player();
+                playerz.playerName = name;
+                playerz.playerNum = num;
+                playerz.playerPos = pos;
+                Players.Add(playerz);
+            }
+        }
+        List<Team> teamlist = new List<Team>();
+        List<string> countrylist = new List<string>();
+        public void addingteam(string name, string country, string city)
+        {
+            if (teamlist.Any(tim => tim.TeamName == name))
+            {
+                MessageBox.Show("Team Already Exists", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!countrylist.Contains(country))
+            {
+                countrylist.Add(country);
+                comboBoxcountry.Items.Add(country);
+            }
+            Team newTeam = new Team { TeamName = name, TeamCountry = country, TeamCity = city };
+            teamlist.Add(newTeam);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxpos.Items.Add("GK");
+            comboBoxpos.Items.Add("DF");
+            comboBoxpos.Items.Add("MF");
+            comboBoxpos.Items.Add("FW");
+            addingteam("Japan National Team", "Japan", "Tokyo");
+            addingteam("Manchester City", "England", "Manchester");
+            addingteam("Brazil National Team", "Brazil", "Brazil");
+
+            foreach (Team tim in teamlist)
+            {
+                if (tim.TeamName == "Japan National Team")
+                {
+                    tim.addingplayer("Eiji Kawashima", "1", "GK");
+                    tim.addingplayer("Yuto Nagatomo", "5", "DF");
+                    tim.addingplayer("Maya Yoshida", "22", "DF");
+                    tim.addingplayer("Gen Shoji", "3", "DF");
+                    tim.addingplayer("Hiroki Sakai", "19", "DF");
+                    tim.addingplayer("Gaku Shibasaki", "16", "MF");
+                    tim.addingplayer("Makoto Hasebe", "17", "MF");
+                    tim.addingplayer("Takashi Inui", "8", "MF");
+                    tim.addingplayer("Shinji Kagawa", "10", "MF");
+                    tim.addingplayer("Yuya Osako", "18", "FW");
+                    tim.addingplayer("Keisuke Honda", "4", "FW");
+                }
+            }
+
+            foreach (Team tim in teamlist)
+            {
+                if (tim.TeamName == "Manchester City")
+                {
+                    tim.addingplayer("Ederson", "31", "GK");
+                    tim.addingplayer("Joao Cancelo", "27", "DF");
+                    tim.addingplayer("John Stones", "05", "DF");
+                    tim.addingplayer("Ruben Dias", "03", "DF");
+                    tim.addingplayer("Kyle Walker", "02", "DF");
+                    tim.addingplayer("Fernandinho", "25", "MF");
+                    tim.addingplayer("Rodri", "16", "MF");
+                    tim.addingplayer("Kevin De Bruyne", "17", "MF");
+                    tim.addingplayer("Riyad Mahrez", "26", "FW");
+                    tim.addingplayer("Phil Foden", "47", "FW");
+                    tim.addingplayer("Erling Haaland", "09", "FW");
+                }
+            }
+
+            foreach (Team tim in teamlist)
+            {
+                if (tim.TeamName == "Brazil National Team")
+                {
+                    tim.addingplayer("Alisson Becker", "1", "GK");
+                    tim.addingplayer("Danilo", "2", "DF");
+                    tim.addingplayer("Thiago Silva", "3", "DF");
+                    tim.addingplayer("Marquinhos", "4", "DF");
+                    tim.addingplayer("Alex Sandro", "6", "DF");
+                    tim.addingplayer("Casemiro", "5", "MF");
+                    tim.addingplayer("Fabinho", "8", "MF");
+                    tim.addingplayer("Philippe Coutinho", "11", "MF");
+                    tim.addingplayer("Neymar Jr", "10", "FW");
+                    tim.addingplayer("Roberto Firmino", "20", "FW");
+                    tim.addingplayer("Gabriel Jesus", "9", "FW");
+                }
+            }
+        }
+        private List<Team> teams = new List<Team>();
+        private void buttonaddteam_Click(object sender, EventArgs e)
+        {
+            string name = textBoxteamname.Text.Trim();
+            string country = textBoxteamcountry.Text.Trim();
+            string city = textBoxteamcity.Text.Trim();
+
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(country) || string.IsNullOrEmpty(city))
+            {
+                MessageBox.Show("All Of The Fields Need To Be Filled", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (teamlist.Any(t => t.TeamName == name))
+            {
+                MessageBox.Show("Team Already Exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var team = new Team()
+            {
+                TeamName = name,
+                TeamCountry = country,
+                TeamCity = city
+            };
+
+            teamlist.Add(team);
+
+            if (!countrylist.Contains(country))
+            {
+                countrylist.Add(country);
+                comboBoxcountry.Items.Add(country);
+            }
+
+            textBoxteamname.Text = "";
+            textBoxteamcountry.Text = "";
+            textBoxteamcity.Text = "";
+        }
+        private void buttonaddplayer_Click(object sender, EventArgs e)
+        {
+            if (textBoxplayername.Text == "" || textBoxplayernumber.Text == "" || comboBoxpos.SelectedIndex < 0)
+            {
+                MessageBox.Show("All Of The Fields Need To Be Filled", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                foreach (Team tim in teamlist)
+                {
+                    if (tim.TeamName == comboBoxteam.SelectedItem.ToString())
+                    {
+                        tim.addingplayer(textBoxplayername.Text, textBoxplayernumber.Text, comboBoxpos.SelectedItem.ToString());
+                        int cekplayernamedouble = 0;
+                        int cekplayernumdouble = 0;
+                        foreach (Player pemain in tim.Players)
+                        {
+                            if (pemain.playerName == textBoxplayername.Text)
+                            {
+                                cekplayernamedouble++;
+                            }
+                            if (pemain.playerNum == textBoxplayernumber.Text)
+                            {
+                                cekplayernumdouble++;
+                            }
+                        }
+                        if (cekplayernamedouble > 1)
+                        {
+                            tim.Players.RemoveAt(tim.Players.Count - 1);
+                            MessageBox.Show("Player With The Same Name Found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else if (cekplayernumdouble > 1)
+                        {
+                            tim.Players.RemoveAt(tim.Players.Count - 1);
+                            MessageBox.Show("Player With The Same Number Found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            comboBoxpos.SelectedIndex = -1;
+            textBoxplayername.Text = "";
+            textBoxplayernumber.Text = "";
+            listBox1.Items.Clear();
+            foreach (Team tim in teamlist)
+            {
+                if (tim.TeamName == comboBoxteam.SelectedItem.ToString())
+                {
+                    foreach (Player pemain in tim.Players)
+                    {
+                        listBox1.Items.Add($"(" + pemain.playerNum + ")" + " " + pemain.playerName + ", " + pemain.playerPos);
+                    }
+                }
+            }
+            listBox1.Sorted = true;
+        }
+        private void comboBoxcountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCountry = comboBoxcountry.SelectedItem.ToString();
+            comboBoxteam.Items.Clear();
+            foreach (var team in teamlist.Where(t => t.TeamCountry == selectedCountry))
+            {
+                comboBoxteam.Items.Add(team.TeamName);
+            }
+        }
+
+
+        private void comboBoxteam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (Team tim in teamlist)
+            {
+                if (tim.TeamName == comboBoxteam.SelectedItem.ToString())
+                {
+                    foreach (Player pemain in tim.Players)
+                    {
+                        listBox1.Items.Add($"(" + pemain.playerNum + ")" + " " + pemain.playerName + ", " + pemain.playerPos);
+                    }
+                }
+            }
+            listBox1.Sorted = true;
+        }
+
+        private void buttonremove_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 11)
+            {
+                MessageBox.Show("Unable To Remove Players If Players Less Than Equal 11", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (listBox1.SelectedItem == null || comboBoxteam.SelectedItem == null)
+                {
+                    MessageBox.Show("Please select a team and a player to remove.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                Team selectedTeam = teamlist.FirstOrDefault(t => t.TeamName == comboBoxteam.SelectedItem.ToString());
+                if (selectedTeam == null)
+                {
+                    MessageBox.Show("Selected team not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                Player selectedPlayer = selectedTeam.Players.FirstOrDefault(p => $"({p.playerNum}) {p.playerName}, {p.playerPos}" == listBox1.SelectedItem.ToString());
+                if (selectedPlayer == null)
+                {
+                    MessageBox.Show("Selected player not found in the selected team.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                selectedTeam.Players.Remove(selectedPlayer);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                listBox1.Sorted = true;
+            }
+        }
+    }
+}
